9890ae3d6a87ac71d57ed3571b5af52877d64bc4
Esmeralda López <lg.esme@hotmail.com>
2018-06-08 23:51:10 -0600

    Cambio en la estructura del proyecto

---
+++ SIG_LaFamiliar/SIG_LaFamiliar/FormLogin.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace SIG_LaFamiliar
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.MaximumSize = new System.Drawing.Size(800, 400);
+            this.MinimumSize = new System.Drawing.Size(800, 400);
+            this.Name = "frmLogin";
+            this.Text = "Sistema de Información Gerencial para el Apoyo al Análisis de la Morosidad";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
